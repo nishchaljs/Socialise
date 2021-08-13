bacf8bafd8cbf89552ed62eb5c142d084309fdfa
Nishchal J <nishchaljs@gmail.com>
2021-08-13 22:49:05 +0530

    first half

--- src/components/Spinner/Spinner.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Spinner.scss";
-
-const Spinner = () => {
-    return <div className="loader__box"><div className="loader" /></div>;
-};
-
-export default Spinner;
